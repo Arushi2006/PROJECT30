fd44ac5c6e015e0ddd281636541237e667333bbe
Arushi2006 <72396009+Arushi2006@users.noreply.github.com>
2020-11-10 08:16:22 -0800

    Add files via upload

---
+++ box.js
@@ -0,0 +1,29 @@
+class Box
+{
+  constructor(x,y,width,height)
+  {
+    var options=
+    {
+      restitution:0.7,
+      density:0.3
+    }
+    this.body=Bodies.rectangle(x,y,width,height,options);
+    this.width=width;
+    this.height=height;
+    World.add(world,this.body);
+    this.Visibility=255;
+  }  
+  display()
+  {
+      if(this.body.speed<3)
+      {
+      World.remove(world,this.body);
+      push()
+      this.Visibility=this.Visibility-5;
+      tint(255,this.Visibility);
+      fill(random(0,255),random(0,255),random(0,255));
+      rect(this.body.position.x,this.body.position.y,this.width,this.height);
+      pop();
+      }
+     }
+}
